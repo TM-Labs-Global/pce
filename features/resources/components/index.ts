7651ea4ab4fd634d983e030bb422615fbe581343
Raphael <casmir2000@gmail.com>
2026-09-01 17:32:40 +0100

    added images of individual products

--- features/resources/components/index.ts
+++ features/resources/components/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from './resources-hero';
+export * from './reports';
--- features/resources/pages/resources-page.tsx
+++ features/resources/pages/resources-page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import React from 'react';
+import { ResourcesHero, Reports } from '../components';
 
 export const ResourcesPage = () => {
   return (
     <div className="flex flex-col w-full bg-[var(--color-canvas-tint)] min-h-screen">
-      {/* Add your feature components here */}
+      <ResourcesHero />
+      <Reports />
     </div>
   );
 };
